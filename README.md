eda7a6e9ad059793d61db8b457c76f171a0663ba
maciekkossuda <maciek@kossuda.pl>
2014-04-02 14:50:55 +0200

    readme

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ magento-tips
 
 * rest api filtering
 * not confirmed customer collection
+* save one attribute
--- save-attribute.php
+++ save-attribute.php
@@ -1,5 +1,7 @@
 <?php
 // save only one attribute (EAV models)
+// http://stackoverflow.com/questions/6714588/save-an-attribute-value-without-saving-its-parent-entity-in-magento#answer-8164012
+// via Vinai
 $product = Mage::getModel('catalog/product')->load(1);
 $product
   ->setSomeAttr('value')
